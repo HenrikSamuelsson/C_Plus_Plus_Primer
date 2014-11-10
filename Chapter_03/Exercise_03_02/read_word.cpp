54b82bab169129e80a86d427df31075e5c14015b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-11-10 21:26:39 +0100

    Committing exercise 3.2, part two.

---
+++ Chapter_03/Exercise_03_02/read_word.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+    string input;
+    while(cin >> input)
+    {
+        cout << input << endl;
+    }
+    return 0;
+}
