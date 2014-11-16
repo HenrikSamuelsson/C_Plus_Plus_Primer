eb37f4cf78d1e9bccf1180cccdb7a0fb87c8af0a
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-11-16 13:41:05 +0100

    Committing exercise 3.41.

---
+++ Chapter_03/Exercise_03_41/vector_from_array.cpp
@@ -0,0 +1,30 @@
+/**
+ * \file
+ *      vector_from_array.cpp
+ * \author
+ *      Henrik Samuelsson
+ */
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::begin;
+using std::end;
+using std::cout;
+
+/**
+ * /brief
+ *    Initializes a vector based on data in an array.  
+ */
+int main() {
+    int int_array[] = {0, 1, 2, 3, 4, 5};
+    vector<int> ivec(begin(int_array), end(int_array));
+    
+    // Test the program by printing the content in ivec.
+    for(int i:ivec)
+        cout << i << " ";
+    
+    return 0;
+}
+
