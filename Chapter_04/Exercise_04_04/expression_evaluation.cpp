a8b9d98ce08e98087c530d46fde8559fd99d1dd0
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-11-19 19:31:39 +0100

    Committing exercise 4.4.

---
+++ Chapter_04/Exercise_04_04/expression_evaluation.cpp
@@ -0,0 +1,19 @@
+/**
+ * \file
+ *      expression_evaluation.cpp
+ * \author
+ *      Henrik Samuelsson
+ */
+#include <iostream>
+
+using std::cout;
+
+/**
+ * /brief
+ *    Initializes a vector based on data in an array.  
+ */
+int main() {
+    cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2;  
+    return 0;
+}
+
