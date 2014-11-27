e905b75901813b3f4b929278151d700daeb996ca
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-11-27 22:14:05 +0100

    Committing exercise 2.8.

---
+++ Chapter_02/Exercise_02_08/escape_sequences.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file
+ *      escape_sequences.cpp
+ * @author
+ *      Henrik Samuelsson
+ */
+#include <iostream>
+
+using std::cout;
+
+/**
+ * @brief
+ *      Print that include special characters.
+ */
+int main() {
+    cout << "2M\n";
+    cout << "2\tM\n";
+    return 0;
+}
+
