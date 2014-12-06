fce6342b126a3026fea54610f73038bbdd857373
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-06 15:16:58 +0100

    Committing exercise 10.1.

---
+++ Chapter_10/Exercise_10_01/count_ints.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file
+ *      count_ints.cpp
+ * \author
+ *      Henrik Samuelsson
+ */
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::count;
+
+const int ANSWER_TO_THE_ULTIMATE_QUESTION = 42;
+
+/**
+ * @brief
+ *      Counts number of occurrences of given int in a vector of int's.
+ */
+int main() {
+    // Generate 200 pseudo random numbers and store them in a vector.
+    vector<int> v;
+    while(v.size() < 200) {
+        v.push_back(rand() % 100);
+    }
+    
+    int c;
+    c = count(v.begin(), v.end(), ANSWER_TO_THE_ULTIMATE_QUESTION);
+    cout << ANSWER_TO_THE_ULTIMATE_QUESTION << " was found " << c << " times.";
+    
+    return 0;
+}
