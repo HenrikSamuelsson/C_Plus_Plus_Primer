92c8b3ff16d0494c716d13684c2000fce547f0e8
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2014-12-20 13:47:58 +0100

    Update output_test.cpp

--- Chapter_01/Exercise_01_08/output_test.cpp
+++ Chapter_01/Exercise_01_08/output_test.cpp
@@ -12,7 +12,7 @@ int main ()
 {
     std::cout << "*/";  // Ok will print */
     std::cout << "*/";  // Ok will print */
-    std::cout << /* "*/" */;     // Will not compile the first citation mark 
+    std::cout << /* "*/" */;    // Will not compile the first citation mark 
                                 // ends up inside a comment.
     std::cout << /* "*/" /* "/*" */;    // Ok will print  */ 
     return 0;
