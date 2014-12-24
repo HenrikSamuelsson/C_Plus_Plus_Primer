5191fe20e1789e87df41b21877f2aa79ba033b20
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-24 17:22:16 +0100

    Added som comments for exercise 1.21.

--- Chapter_01/Exercise_01_21/sum_two_sales_data.cpp
+++ Chapter_01/Exercise_01_21/sum_two_sales_data.cpp
@@ -7,6 +7,16 @@
 #include <iostream>
 #include "Sales_item.h"
 
+/**
+ * @brief
+ *      Reads two sales_item's objects prints the sum.
+ * @note
+ *      The exercise instructions in the book state that the two objects have
+ *      the same ISBN. It is assumed that it is up to the user to enter objects
+ *      with the same ISBN, the program will not check this.
+ * @return 
+ *      0 upon correct and completed execution 
+ */
 int main() {
     Sales_item item1, item2;
     std::cin >> item1 >> item2;
