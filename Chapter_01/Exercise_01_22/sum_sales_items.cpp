42b44ca8c34370c929cbecee9bd6dbea45de788a
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-24 18:36:13 +0100

    Committing Exercise 1.22.

--- Chapter_01/Exercise_01_22/sum_sales_items.cpp
+++ Chapter_01/Exercise_01_22/sum_sales_items.cpp
@@ -7,6 +7,17 @@
 #include <iostream>
 #include "Sales_item.h"
 
+/**
+ * @brief
+ *      Reads a number of sales_item's objects prints the sum.
+ * @note
+ *      The exercise instructions in the book state that the objects have
+ *      the same ISBN. It is assumed that it is up to the user to enter objects
+ *      with the same ISBN, the program will not check this.
+ * @return 
+ *      0 upon correct and completed execution 
+ */
+
 int main () {
     Sales_item sum, first, next;
     
@@ -18,5 +29,6 @@ int main () {
         sum += next;
     }
     std::cout << sum;
+    
     return 0;
 }
