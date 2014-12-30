799c4768d7d00ddae24411d1c5b8837324d451aa
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-30 15:10:03 +0100

    Committing exercise 3.1 part 1.

---
+++ Chapter_03/Exercise_03_01/Exercise_03_01_part_1.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file
+ *      exercise_03_01_part_1.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.somuelsson(at)gmail.com
+ * @brief
+ *      Rewrite of an example program from paragraph 1.4.1, with appropriate
+ *      using declarations.
+ */
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    int sum = 0, val = 1;
+    // Keep executing the while as long as val is less than or equal to 10.
+    while (val <= 10) {
+        sum += val;     // Assigns sum + val to sum.
+        ++val;          // Add 1 to val.
+    }
+    cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+}
