cc58bff4d65075a4e1b9d4d467c90cdeae4476c5
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-31 00:27:35 +0100

    Committing exercise 3.4 part 1.

---
+++ Chapter_03/Exercise_03_04/exercise_03_04_part_1.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file
+ *      exercise_03_04_part_1.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+/**
+ * @brief
+ *      Reads two strings and checks whether they are equal.
+ * @details
+ *      Will report which of the two strings that is larger, in the case where 
+ *      the strings are unequal.
+ * @return 
+ *      0 Upon correct program completion. 
+ */
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if(s1 == s2) {
+        cout << "The two strings are equal.";
+    } else if(s1 > s2) {
+        cout << "The first string is larger than the second";
+    } else {
+        cout << "The first string is smaller than the second";
+    }
+    
+    return 0;
+}
