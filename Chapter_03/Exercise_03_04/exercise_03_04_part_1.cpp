ead3b97d80de4ec09f91f29688ff833f33d22a2b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-31 00:38:08 +0100

    Committing exercise 3.4 part 1.

--- Chapter_03/Exercise_03_04/exercise_03_04_part_1.cpp
+++ Chapter_03/Exercise_03_04/exercise_03_04_part_1.cpp
@@ -17,6 +17,8 @@ using std::string;
  * @details
  *      Will report which of the two strings that is larger, in the case where 
  *      the strings are unequal.
+ * @note 
+ *      Can not handle strings of zero length.
  * @return 
  *      0 Upon correct program completion. 
  */
@@ -27,9 +29,9 @@ int main() {
     if(s1 == s2) {
         cout << "The two strings are equal.";
     } else if(s1 > s2) {
-        cout << "The first string is larger than the second";
+        cout << "The first string is larger than the second.";
     } else {
-        cout << "The first string is smaller than the second";
+        cout << "The first string is smaller than the second.";
     }
     
     return 0;
