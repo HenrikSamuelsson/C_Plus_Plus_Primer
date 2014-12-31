174728b25aa920e19c356378d08966d3e67200c1
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-31 15:56:40 +0100

    Fixed spelling.

--- Chapter_03/Exercise_03_05/exercise_03_05_part_1.cpp
+++ Chapter_03/Exercise_03_05/exercise_03_05_part_1.cpp
@@ -2,7 +2,7 @@
  * @file
  *      exercise_03_05_part_1.cpp
  * @author
- *      Henrik Samuelsson, henrik.somuelsson(at)gmail.com
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
  */
 #include <iostream>
 #include <string>
