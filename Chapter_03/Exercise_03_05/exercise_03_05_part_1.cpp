912f192f81ec3d6d8b51b8d40ab2e9c1c605bdd6
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-12-31 01:05:43 +0100

    Renaming.

---
+++ Chapter_03/Exercise_03_05/exercise_03_05_part_1.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file
+ *      exercise_03_05_part_a.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.somuelsson(at)gmail.com
+ */
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int main() {
+    string temp, result;
+    while(cin >> temp) {
+        result.append(temp);
+    }
+    cout << result;
+    return 0;
+}
