1bd1c8c6c364752e04c7b34e21f0c03e01dc83c8
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-01-02 19:03:17 +0100

    Update exercise_03_23.cpp

--- Chapter_03/Exercise_03_23/exercise_03_23.cpp
+++ Chapter_03/Exercise_03_23/exercise_03_23.cpp
@@ -13,7 +13,7 @@ using std::vector;
 int main() {
     vector<int> i_vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     
-    // Assign each element a value that is twice its current value.
+    // Assign each element in the vector a value that is twice its current value.
     for (auto it = i_vec.begin(); it != i_vec.end(); ++it) {
         *it = 2 * *it;
     }
