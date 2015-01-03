eac32853da08a7e19451a88c608037101945c954
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-03 12:31:12 +0100

    Committing exercise 11.3.

---
+++ Chapter_11/Exercise_11_03/exercise_11_03.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ *      exercise_11_03.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+#include <map>
+#include <iostream>
+#include <string>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::map;
+using std::string;
+
+int main() {
+    map<string, size_t> word_count; // Empty map from string to size_t.
+    string word;
+    
+    // Get input words from the user of the program.
+    while (cin >> word) {
+        ++word_count[word];    // Fetch and increment the counter for word.
+    }
+    
+    // Print the result of the word count.
+    for (const auto &w : word_count) {  // For each element in word count.
+        cout << w.first << " occurs " << w.second 
+                << ((w.second < 1) ? " times" : " time") << endl;
+    }
+    return 0;
+}
