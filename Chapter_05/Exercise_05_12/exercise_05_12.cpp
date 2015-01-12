7a604563e221cabb3d98cbdce8cf926dfba6e435
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-01-12 20:24:39 +0100

    Update exercise_05_12.cpp

--- Chapter_05/Exercise_05_12/exercise_05_12.cpp
+++ Chapter_05/Exercise_05_12/exercise_05_12.cpp
@@ -4,7 +4,7 @@
  * @author
  *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
  * @brief
- *      Exercise 5.12 from the book C++ Primer (5th edition)
+ *      Exercise 5.12 from the book C++ Primer (5th edition).
  * @details
  *      Modify our vowel-counting program so that it counts the number of 
  *      occurrences of the following two-character sequences: ff, fl, fi.
