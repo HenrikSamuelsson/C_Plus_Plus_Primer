406a74445a0b117b058dfaee544e42ef3d89f38f
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-01-13 23:54:05 +0100

    Update and rename product.cpp to exercise_01_04.cpp

--- Chapter_01/Exercise_01_04/exercise_01_04.cpp
+++ Chapter_01/Exercise_01_04/exercise_01_04.cpp
@@ -1,4 +1,13 @@
+/**
+ * @file
+ *      exercise_01_04.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 1.4 from the book C++ Primer (5th edition).
+ */
 #include <iostream>
+
 /**
  * Multiplies two user defined integers.
  * 
@@ -6,8 +15,8 @@
  */
 int main()
 {
-    std::cout << "Enter two numbers:" << std::endl;
     int v1 = 0, v2 = 0;
+    std::cout << "Enter two numbers:" << std::endl;
     std::cin >> v1 >> v2;
     std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2
             << std::endl;
