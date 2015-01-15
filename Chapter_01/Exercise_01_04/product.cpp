98a8d8fa5a1eeab9d39a7411588e6c248ceb9752
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-15 22:35:38 +0100

    committing product.cpp before merge

--- Chapter_01/Exercise_01_04/product.cpp
+++ Chapter_01/Exercise_01_04/product.cpp
@@ -9,7 +9,7 @@ int main()
     std::cout << "Enter two numbers:" << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2
+    std::cout << "The product of " << v1; << " and " << v2 << " is " << v1 * v2
             << std::endl;
     return 0;
 }
