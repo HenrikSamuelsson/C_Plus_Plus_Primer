11a76ba9af31a9d33e538267fc090d7f8fec326b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-17 14:44:30 +0100

    Inital commit of exercise 5.23.

---
+++ Chapter_05/Exercise_05_23/exercise_05_23.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file
+ *      exercise_05_23.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 5.23 from the book C++ Primer (5th edition).
+ * @details
+ *      Write a program that reads two integers from the standard input and 
+ *      prints the result of dividing the first number by the second.     
+ */
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int dividend, divisor, quotient, remainder;
+
+    cout << "Enter two integers that will be used for divison: ";
+    cin >> dividend >> divisor;
+    
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+    
+    cout << endl;
+    cout << dividend << " divided by " << divisor << " yields:" << endl;
+    cout << "\t" <<"quotient = " << quotient << endl;
+    cout << "\t" <<"remainder = " << remainder << endl;
+
+    return 0;
+}
