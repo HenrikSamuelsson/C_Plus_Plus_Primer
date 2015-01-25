cf751bf40482c4320f25f24e4493a7c178898bad
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-25 18:44:36 +0100

    Commit of Quote.h.

---
+++ Chapter_15/Exercise_15_03/Quote.h
@@ -0,0 +1,30 @@
+/**
+ * @file
+ *      Quote.h
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 5.15 from the book C++ Primer (5th edition).
+ * @details
+ *      Define your own version of the Quote class and the print_total function.
+ */
+#ifndef QUOTE_H
+#define QOUTE_H
+
+#include <string>
+
+class Quote {
+public:
+    Quote() = default;
+    Quote(const std::string& isbn, double sales_price): isbnNo(isbn), 
+    price(sales_price) { }
+    std::string isbn() const { return isbnNo; }
+    virtual double net_price(std::size_t n) const { return n * price; }
+    virtual ~Quote() = default;
+private:
+    std::string isbnNo;
+protected:
+    double price = 0.0;
+};
+
+#endif
