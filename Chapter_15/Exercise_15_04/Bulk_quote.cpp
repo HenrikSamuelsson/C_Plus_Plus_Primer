2fab84e23d704c04b26a180d9b1f5398dd3a94ed
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-26 21:27:37 +0100

    Bug fix.

--- Chapter_15/Exercise_15_04/Bulk_quote.cpp
+++ Chapter_15/Exercise_15_04/Bulk_quote.cpp
@@ -12,6 +12,6 @@ double Bulk_quote::net_price(std::size_t n) const {
     if (n >= min_qty) {
         return n * (1 - discount) * price; 
     } else {
-        return cnt * price;
+        return n * price;
     }
 }
