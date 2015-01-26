f59a4e4fa18e82a6edbe79381c6873897f62d009
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-01-26 18:12:39 +0100

    Commit Bulk_quote.h for exercise 15.4.

---
+++ Chapter_15/Exercise_15_04/Bulk_quote.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *      Bulk_quote.h
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 15.5 from the book C++ Primer (5th edition).
+ * @details
+ *      Define your own version of the Bulk_quote class.
+ */
+#ifndef QUOTE_H
+#define QOUTE_H
+
+#include <string>
+#include "Quote.h"
+
+class Bulk_quote : public Quote {
+public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string& isbn, double p, 
+            std::size_t q, double d) : Quote(isbn, p), mint_qty(q), discount(d);
+    
+    double net_price(std::size_t n) const override;
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
+};
+
+#endif
