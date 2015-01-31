5f1c9e4ae28f2d725158548bc135ea678bee36c7
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-01-31 15:22:45 +0100

    Initial of commit exercise 2.41.

---
+++ Chapter_02/Exercise_02_41/exercise_02_41_part_1.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file 
+ *      exercise_02_41_part_1.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *       Part one of exercise 2.41 from the book C++ Primer (5th edition).
+ * @details
+ *      A program that reads a set of book sales transactions, writing each
+ *      transaction to the standard output.
+ */
+#include <iostream>
+
+struct Sales_data {
+  std::string isbn;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
+};
+
+int main() {
+    Sales_data sd;
+    
+    while(std::cin >> sd.isbn >> sd.units_sold >> sd.revenue) {
+        std::cout << "ISBN: " << sd.isbn  << std::endl;
+        std::cout << "Units sold: " << sd.units_sold  << std::endl;
+        std::cout << "Revenue: " << sd.revenue  << std::endl;
+    }
+            
+    return 0;
+}
