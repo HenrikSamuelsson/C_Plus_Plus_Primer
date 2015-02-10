6ba0983113dbe4c0f5c36d715c3701a9b6cb0f79
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-02-10 22:43:29 +0100

    Initial commit of exercise 7.4.

---
+++ Chapter_07/Exercise_07_04/Person.h
@@ -0,0 +1,21 @@
+/**
+ * @file
+ *      Person.h
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 7.4 from the book C++ Primer (5th edition).
+ */
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    string address;
+};
+
+#endif
