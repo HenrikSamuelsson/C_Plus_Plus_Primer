d6d613d4297019855e6dec8f5fa3c9a5e2e18c7b
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-03-06 11:48:43 +0100

    Update and rename main.cpp to exercise_01_01.cpp

---
+++ Chapter_01/Exercise_01_01/exercise_01_01.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ *      exercise_01_01.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 1.1 from the book C++ Primer (5th edition).
+ */
+
+/**
+ * @brief
+ *      Dummy program only used to verify the development environment setup.
+ * @return 
+ *      0 upon execution success 
+ */
+int main ()
+{
+    return 0;
+}
--- Chapter_01/Exercise_01_01/main.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * Dummy program used to verify the development environment setup.
- * 
- * @return 0 if the program runs successfully 
- */
-int main ()
-{
-    return 0;
-}
