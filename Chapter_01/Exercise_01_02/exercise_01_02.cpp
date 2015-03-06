5dd3dc51091ac51429785f64d3a27e90f692d78d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-03-06 11:55:44 +0100

    Update exercise_01_02.cpp

--- Chapter_01/Exercise_01_02/exercise_01_02.cpp
+++ Chapter_01/Exercise_01_02/exercise_01_02.cpp
@@ -1,9 +1,15 @@
 /**
  * @file
- *      exercis_01_02.cpp
- * Testing with return of -1 from main, to see how the system reacts.
- * 
- * @return -1
+ *      exercise_01_02.cpp
+ * @author
+ *      Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ * @brief
+ *      Exercise 1.2 from the book C++ Primer (5th edition).
+ * @details
+ *      Exercise instructions: Change the program to return -1. A return value
+ *      of -1 is often treated as an indicator that the program failed. Recompile
+ *      and return your program to see how your system treats a failure indicator
+ *      from main.
  */
 int main ()
 {
